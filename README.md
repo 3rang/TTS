aaf0d6a28c020a50f380fff0e0c21b6d2c1cee93
Tarang Patel <tarang3399.patel@gmail.com>
2021-10-10 11:16:26 +0100

    chore: tts with dockerfile and working tts app

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# TTS
-Deep Learning Based Text-to-Speech for Embedded Device
+# NAME
+
+## Project Structure
+This is the current structure of the project
+```shell
+├── app                 # C++ based speech engine
+│   ├── CMakeLists.txt
+│   ├── include         # Top level includes
+│   ├── python-tests    # Component Tests in python
+│   ├── src             # Source code
+│   │   ├── asr         # Automated Speech Recognizer code
+│   │   ├── nlu         # Natural Language Understanding code
+│   │   ├── spotter     # Keyword Spotting code
+│   │   └── tts         # Text to Speech code
+│   │   └── aal         # Audio Abstraction Layer code
+│   │   └── mercies     # Actual application
+│   └── systemd         # Systemd related files
+├── Dockerfile
+├── docs                # Markdown style documentation
+├── explore             # Experiments run in python
+│   └── notebooks       # Jupyter Notebooks
+├── Jenkinsfile
+├── README.md
+└── scripts             # Helper scripts
+```
+
+# Setup Docker 
+
+```shell
+
+# Create a workspace for your work
+mkdir workspace-name
+cd workspace-name
+
+# clone the repo.
+git clone ssh://git@git.swf.daimler.com:7999/mbient/mercies.git
+cd mercies/
+
+# Build the docker container to setup development environment
+docker build -f Dockerfile -t tts .
+ 
+# run docker 
+docker run -v $PWD:/home/tts/mercies --network host -it tts:latest
+
+```
+
+# Build the Application 
+
+# High Level Arquitecture
+
+## More coming soon..
---
+++ src/tts/include/ttsService.hpp
@@ -0,0 +1,85 @@
+
+// SPDX-FileCopyrightText: 2021 MBition GmbH
+//
+// SPDX-License-Identifier: Closed software
+
+
+#ifndef INC_TTSSERVICE_HPP_
+#define INC_TTSSERVICE_HPP_
+
+#include <iostream>
+#include <vector>
+#include <cstdint>
+#include <cstring>
+#include <string>
+#include <chrono>
+#include <fstream>
+#include <tensorflow/lite/interpreter.h>
+#include <tensorflow/lite/kernels/register.h>
+#include <tensorflow/lite/model.h>
+#include <tensorflow/lite/optional_debug_tools.h>
+
+typedef struct
+{
+	float *mel_Data;
+	std::vector<int32_t> mel_Shape;
+	int32_t mel_bytes;
+} MelGenData;
+
+
+#define TFLITE_MINIMAL_CHECK(x)                              \
+	if (!(x)) {                                                \
+		fprintf(stderr, "Error at %s:%d\n", __FILE__, __LINE__); \
+		exit(1);                                                 \
+	}
+
+class ttsBase
+{
+
+	public:
+
+		std::unique_ptr<tflite::FlatBufferModel> model;
+		std::unique_ptr<tflite::Interpreter> interpreter;
+		tflite::ops::builtin::BuiltinOpResolver resolver;
+
+};
+
+
+class ttsBaseMel:public ttsBase
+{
+
+	private:
+
+		const char* melgenPath;
+
+	public:
+		ttsBaseMel(const char* melgenfile): melgenPath(melgenfile)
+	{
+		std::cout << "TTSModel melgenPath" << std::endl;
+	};
+
+		MelGenData interpreterBuildMel(std::vector<int32_t> &localbuf);
+
+};
+
+
+class ttsBasevocodoer:public ttsBase
+{
+
+	private:
+
+		const char* vocoderPath;
+
+	public:
+
+		ttsBasevocodoer(const char* vocoderfile): vocoderPath(vocoderfile)
+	{
+		std::cout << "TTSModel vocoderfile" << std::endl;
+	};
+
+		std::vector<float> interpreterBuildvocoder(MelGenData &localbufV);
+
+};
+
+
+#endif /* INC_TTSSERVICE_HPP_ */
